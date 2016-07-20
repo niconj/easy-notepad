2a62f2f699d0ddf0734dac3409d339ef553b4af9
GMaiolo <guille.maiolo@gmail.com>
2016-07-20 00:59:56 -0300

    finished v1.0

--- src/app/note/note.controller.js
+++ src/app/note/note.controller.js
@@ -1,32 +1,34 @@
 export class NoteController {
-  constructor ($scope, 
-    SelectionService, 
-    NotesManager, 
-    $location) {
-    'ngInject';
-    
-    $scope.selection = SelectionService;
-    $scope.readOnly = {
-      title: true, 
-      content: true
-    };
+    constructor ($scope, 
+        SelectionService, 
+        NotesManager, 
+        $location, 
+        $log, 
+        ThemeManager) {
+        'ngInject';
 
-    function init() {
-      if(!SelectionService.note) return $location.path('/');
-      if(NotesManager.isEmpty(SelectionService.note)) { // probably a new one
-        $scope.readOnly.title = false;
-        $scope.readOnly.content = false;
-      }
+        init();
+
+        $scope.modified = () => NotesManager.saveNote(SelectionService.note);
+
+        $scope.back = () => {
+            SelectionService.note = null;
+            $location.path('/');
+        }
+
+        function init() {
+            $scope.componentColor = ThemeManager.componentColor;
+            $scope.selection = SelectionService;
+            $scope.readOnly = {
+                title: true, 
+                content: true
+            };
+            if(!SelectionService.note) return $location.path('/');
+            if(NotesManager.isEmpty(SelectionService.note)) { // probably a new one
+                $scope.readOnly.title = false;
+                $scope.readOnly.content = false;
+            }
+        }
+
     }
-
-    $scope.modified = () => NotesManager.saveNote(SelectionService.note);
-
-    $scope.back = () => {
-      SelectionService.note = null;
-      $location.path('/');
-    }
-
-    init();
-
-  }
 }
--- src/app/shared/theme.manager.js
+++ src/app/shared/theme.manager.js
@@ -1,19 +1,30 @@
 export class ThemeManager {
-  constructor () {
+  constructor ($window, $log) {
     'ngInject';
 
+    var ls = $window.localStorage;
     var light = 'light';
     var dark = 'dark';
 
-    this.theme = dark;
+    this.theme = savedTheme() || dark;
 
     /* using a simple toggle for the moment because there are only two themes
     must apply another approach if there are more in the future */
-    this.toggle = () => this.theme = this.theme === dark? light : dark;
+    this.toggle = () => {
+        let newTheme = this.theme === dark? light : dark;
+        ls.setItem('theme', newTheme);
+        return this.theme = newTheme;
+    }
 
     this.backgroundColor = () => this.theme;
 
     this.componentColor = () => this.theme === dark? light : dark;
+
+    function savedTheme() {
+        let savedTheme = ls.getItem('theme');
+        $log.log(savedTheme);
+        return savedTheme;
+    }
     
   }
 }
