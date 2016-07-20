3fcaa6fa74b275e81daff521d34b8e5716f88de0
GMaiolo <guille.maiolo@gmail.com>
2016-07-20 15:06:33 -0300

    fixing gulp errors for font-assets

--- gulp/build.js
+++ gulp/build.js
@@ -89,8 +89,18 @@ gulp.task('other', function () {
     .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
 });
 
+gulp.task('fonts:dist', function () {
+  return gulp.src(conf.paths.src + '/assets/fonts/*.*')
+  .pipe(gulp.dest(conf.paths.dist + '/assets/fonts'));
+});
+
+gulp.task('fonts:tmp', function () {
+  return gulp.src(conf.paths.src + '/assets/fonts/*.*')
+  .pipe(gulp.dest(conf.paths.tmp + '/assets/fonts'));
+});
+
 gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'other']);
+gulp.task('build', ['html', 'other', 'fonts:dist']);
