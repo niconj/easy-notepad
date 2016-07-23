19fb2821f14df9dfb25f59d18ed54ab65e9ba2e8
GMaiolo <guille.maiolo@gmail.com>
2016-07-23 01:17:55 -0300

    integrated search engine and some styling

---
+++ src/app/shared/search.js
@@ -0,0 +1,21 @@
+export class Search {
+  constructor (NotesManager) {
+    'ngInject';
+
+    this.isSearching = false;
+
+    this.queryString = null;
+
+    this.toggleSearch = () => this.isSearching = !this.isSearching;
+
+    this.performSearch = query => {
+        let notes = NotesManager.getNotes();
+        /* ugly hotfix, TODO: refator */
+        if(query === 1) return notes; 
+        /* returning filtered notes or if the query string is empty return them all */
+        let filtered = notes.filter(note => note.title._includes(query) || note.content._includes(query));
+        return filtered;
+    }
+    
+  }
+}
